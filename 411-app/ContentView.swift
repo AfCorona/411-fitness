dd3ab7ca93406810289d52c762e8806bc262a21e
me <me>
2025-12-08 18:38:54 -0800

    Initial Commit

---
+++ 411-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  411-app
+//
+//  Created by Adolfo Corona on 12/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 411-app/_11_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  _11_appApp.swift
+//  411-app
+//
+//  Created by Adolfo Corona on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct _11_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
